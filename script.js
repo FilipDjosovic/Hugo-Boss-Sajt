c7ad98350659b07a46c4dda72978df7c0abc8da4
FilipDjosovic <filipdjosovic3@gmail.com>
2026-04-05 13:25:33 +0200

    Promenjen fajl script

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-function promenaTeme(checkbox)
+const checkBox = document.getElementById('check');
+
+function promenaTeme()
 {
-	if(checkbox.checked)
+	if(checkBox.checked)
 	{
 		document.body.classList.add("svetla_tema");
 	}
 	else
 	{
 		document.body.classList.remove("svetla_tema");
+	}
+}
+
+checkBox.onchange = () => {
+	localStorage.setItem('stanjeCheckBoxa', checkBox.checked);
+	promenaTeme();
+}
+
+window.onload = () => {
+	const sacuvanoStanje = localStorage.getItem('stanjeCheckBoxa');
+	if (sacuvanoStanje !== null)
+	{
+		checkBox.checked = sacuvanoStanje === 'true';
+		promenaTeme();
+	}
+}
+
+window.onstorage = (event) => {
+	if (event.key === 'stanjeCheckBoxa')
+	{
+		checkBox.checked = event.newValue === 'true';
+		promenaTeme();
 	}
 }
